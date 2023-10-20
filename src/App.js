6f2ade7185cd56584302adfb38c81df2dd74b6ea
Admin <admin@example.com>
2023-10-20 06:53:06 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function doLogin(e) {
+    e.preventDefault();
+    const { data } = await axios.post("http://localhost:4000/user/login", {
+      email,
+      password,
+    });
+    window.localStorage.setItem("access-token", data.data.accessToken);
+  }
+
+  async function getPeople() {
+    const accessToken = window.localStorage.getItem("access-token");
+    if (accessToken) {
+      const { data } = await axios.get("http://localhost:4000/people", {
+        headers: {
+          Authorization: accessToken,
+        },
+      });
+      console.log(data);
+    } else {
+      alert("User not logged in");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={doLogin}>
+        <label>
+          Email
+          <input
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </label>
+        <button>Login</button>
+      </form>
+
+      <button onClick={getPeople} type="button">
+        Get People
+      </button>
+    </>
   );
 }
 

cfbd0f63767bd2486358f5193832bec755964408
Admin <admin@example.com>
2023-10-20 07:48:36 +0100

    Send message implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import { useEffect, useState } from "react";
 import SocketIOClient from "socket.io-client";
 
 const socket = SocketIOClient("http://localhost:4000");
 
 function App() {
-  
+  const [chat, setChat] = useState([]);
+  const [message, setMessage] = useState([]);
 
-  return <h1>Messenger</h1>
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setChat([...chat, data]);
+    });
+  }, [chat]);
+
+  function sendMessage(e) {
+    e.preventDefault();
+    socket.emit("send_message", message);
+    setMessage("");
+    setChat([...chat, message]);
+  }
+
+  return (
+    <div>
+      <h1>Messenger</h1>
+
+      <ul>
+        {chat.map((message, index) => {
+          return <li key={index}>{message}</li>;
+        })}
+      </ul>
+
+      <form onSubmit={sendMessage}>
+        <textarea onChange={(e) => setMessage(e.target.value)}></textarea>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
